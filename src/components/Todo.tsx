2d0b7deeaa87b3433a7d51aaa6d744e0123d167d
ppjinhong <pearljh19@gmail.com>
2022-07-04 21:06:02 +0900

    0704.3

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -4,19 +4,31 @@ import Createtodo from "./Createtodo";
 import Todolist from "./Todolist";
 
 function Todo(){
-    const todos = useRecoilValue(todostate);
-    const selectoroutput = useRecoilValue(todoselector);
-    console.log(todos);
-    console.log(selectoroutput);
+    // const todos = useRecoilValue(todostate);
+    const [todo,doing,done] = useRecoilValue(todoselector);
+    
+    console.log([todo,doing,done]);
     
 
     return(
         
             <>
                 <Createtodo />
-              <ul>
+                <hr></hr>
+              <ul><h2>todo</h2>
                   {/* {todos.map(a=>(<Todolist text={a.text} category={a.category} id={a.id}/>))} */}
-                    {todos.map(a=>(<Todolist key={a.id}{...a}/>))}
+                    {/* {todos.map(a=>(<Todolist key={a.id}{...a}/>))} */}
+                    {todo.map((a)=>(<Todolist key={a.id}{...a}/>))}
+              </ul>
+              <hr></hr>
+              <ul><h2>done</h2>
+                  {/* {todos.map(a=>(<Todolist text={a.text} category={a.category} id={a.id}/>))} */}
+                  {done.map((a)=>(<Todolist key={a.id}{...a}/>))}
+              </ul>
+              <hr></hr>
+              <ul><h2>doing</h2>
+                  {/* {todos.map(a=>(<Todolist text={a.text} category={a.category} id={a.id}/>))} */}
+                  {doing.map((a)=>(<Todolist key={a.id}{...a}/>))}
               </ul>
             </>
           );
