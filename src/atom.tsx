7d814ea4f5ef34e180f8b9180ccbccfe0c62d033
ppjinhong <pearljh19@gmail.com>
2022-07-06 20:17:18 +0900

    0706.1

--- src/atom.tsx
+++ src/atom.tsx
@@ -3,23 +3,23 @@ import { atom ,selector} from "recoil";
 export interface Itodo {
     text: string;
     id:number;
-    category: "TO_DO" | "DOING" | "DONE";
+    category: "TO_DO" | "DOING" | "DONE" ;
   }
 export const todostate = atom<Itodo[]>({
     key : "todo",
     default : [],
 });
+export const categorystate = atom({
+    key: "category",
+    default : "TO_DO",  
+})
 
 export const todoselector = selector({
     key: "todoselector",
     get : ({get})=>{
       const todos= get(todostate);
-      return [
-        todos.filter((todo)=>todo.category==="TO_DO"),
-        todos.filter((todo)=>todo.category==="DOING"),
-        todos.filter((todo)=>todo.category==="DONE"),
-        
-      ];
+      const category = get(categorystate);
+      return todos.filter( a => a.category === category);
     }
 
 });
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,35 +1,37 @@
+import React from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
-import { todoselector, todostate } from "../atom";
+import { categorystate, todoselector, todostate } from "../atom";
 import Createtodo from "./Createtodo";
 import Todolist from "./Todolist";
 
 function Todo(){
     // const todos = useRecoilValue(todostate);
-    const [todo,doing,done] = useRecoilValue(todoselector);
-    
-    console.log([todo,doing,done]);
-    
-
+    const todo = useRecoilValue(todoselector);
+    const [category,setcategory] = useRecoilState(categorystate);
+    console.log(todo);
+    const oninputfn = (event : React.FormEvent<HTMLSelectElement>) =>{
+        setcategory(event.currentTarget.value);
+    }
+    console.log(category);
     return(
         
             <>
                 <Createtodo />
                 <hr></hr>
-              <ul><h2>todo</h2>
+              <ul>
+                  {todo.map( a => (<Todolist key={a.id}{...a} />))}
+              </ul>
                   {/* {todos.map(a=>(<Todolist text={a.text} category={a.category} id={a.id}/>))} */}
                     {/* {todos.map(a=>(<Todolist key={a.id}{...a}/>))} */}
-                    {todo.map((a)=>(<Todolist key={a.id}{...a}/>))}
-              </ul>
-              <hr></hr>
-              <ul><h2>done</h2>
-                  {/* {todos.map(a=>(<Todolist text={a.text} category={a.category} id={a.id}/>))} */}
-                  {done.map((a)=>(<Todolist key={a.id}{...a}/>))}
-              </ul>
-              <hr></hr>
-              <ul><h2>doing</h2>
-                  {/* {todos.map(a=>(<Todolist text={a.text} category={a.category} id={a.id}/>))} */}
-                  {doing.map((a)=>(<Todolist key={a.id}{...a}/>))}
-              </ul>
+                    {/* {todo.map((a)=>(<Todolist key={a.id}{...a}/>))} */}
+                   <form>
+                    <select value={category} onInput={oninputfn}>
+                        <option value={"TO_DO"}>todo</option>
+                        <option value={"DOING"}>doing</option>
+                        <option value={"DONE"}>done</option>
+                    </select>
+                </form>
+            
             </>
           );
           
